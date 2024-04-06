5f55c25476210827b75df464d2ac7e11a5c6018a
seevasannivas <seenivaskk18@gmail.com>
2024-04-06 15:48:40 +0530

    first Commit

--- router.js
+++ router.js
@@ -3,6 +3,6 @@ const auth_controller = require('./auth.controller')
 const { verifyToken } = require('./utils')
 
 module.exports = (app)=>{
-    app.use('/api',verifyToken,api_controller),
+    app.use('/api',api_controller),
     app.use('/auth',auth_controller)
 }
